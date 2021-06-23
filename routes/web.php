b73f3ed6fbaa335041a97fed24455f3817c14778
AR1992-Tony <angelo.raimondo92@gmail.com>
2021-06-23 17:51:23 +0200

    modifiche

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\admin_controller\Admin_userController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,15 +16,31 @@ Route::get('/admin', function () {
     return view('admin.index');
 })->middleware(['auth']);
 
-
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/admin/libri', function () {
-    return view('admin.libri');
+Route::get('admin/libri/elenco_libri', function () {
+    return view('admin.libri.elenco_libri');
 })->middleware(['auth']);
 
+Route::get('admin/utenti/elenco_utenti', function () {
+    return view('admin.utenti.elenco_utenti');
+})->middleware(['auth']);
+
+Route::get('admin/prestiti/elenco_prestiti', function () {
+    return view('admin.prestiti.elenco_prestiti');
+})->middleware(['auth']);
+
+Route::get('admin/editori/elenco_editori', function () {
+    return view('admin.editori.elenco_editori');
+})->middleware(['auth']);
+
+Route::get('admin/autori/elenco_autori', function () {
+    return view('admin.autori.elenco_autori');
+})->middleware(['auth']);
+
+Route::resource('admin/utenti/elenco_utenti', Admin_userController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
 
